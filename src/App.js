6167829b352f4c011624003a09947998410ece71
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-09-16 19:12:09 +0800

    Exercise1 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [value, setValue] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My value is {value}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button  onClick={() => {
+            setValue(value + 1)
+        }}>Increase</button>
+        <button  onClick={() => {
+            setValue(value - 1)
+        }}>Decrease</button>
       </header>
     </div>
   );
